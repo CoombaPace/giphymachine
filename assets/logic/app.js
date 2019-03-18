4978050965bd85d330ebcfbc348fde59d31e05d9
Coomba <coombapace@gmail.com>
2019-03-18 15:13:43 -0400

    Added theme and Title fields

--- assets/logic/app.js
+++ assets/logic/app.js
@@ -1,99 +1,103 @@
 
-      // Initial array of giphs
-      var giphs = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
-      // displayGiphRating function re-renders the HTML to display the appropriate content
-      function displayGiphRating() {
+  // Initial array of giphs
+  var giphs = ["Joey Diaz", "Joe Rogan", "George Carlin", "Bill Hicks"];
+  // displayGiphRating function re-renders the HTML to display the appropriate content
+  function displayGiphRating() {
 
-        var searchTerm = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=sNcKr7LScAoiuiJSCdfmJvALAAXq8j89&q=" +
-        searchTerm + "&limit=10&offset=0&lang=en";
-        // Creates AJAX call for the specific giph button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .then(function(response) {
-            console.log(response);
-            var results = response.data;
+    var searchTerm = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=sNcKr7LScAoiuiJSCdfmJvALAAXq8j89&q=" +
+    searchTerm + "&limit=10&offset=0&lang=en";
+    // Creates AJAX call for the specific giph button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then(function(response) {
+        console.log(response);
+        var results = response.data;
 
-            for (var i = 0; i < results.length; i++){
-            
-                // Creates a div to hold the giph
-                let giphDiv = $("<div class='giph-div'>");
-                var rating = results[i].rating;
-                let pOne = $('<p>').text('Rating: ' + rating);
-                giphDiv.append(pOne);
-                
-                let pic = $('<img>').attr({"src": results[i].images.fixed_height_still.url, 
-                                            "data-state": "still",
-                                            "data-still": results[i].images.fixed_height_still.url,
-                                            "data-animate": results[i].images.fixed_height.url,
-                                            "class": "gif"});
-
-                let picAni = $('<img>').attr("src", results[i].images.fixed_height.url);
-
-                console.log(results);
-                
-                giphDiv.append(pic);
-
-                 $("#giph-container").prepend(giphDiv);
+        for (var i = 0; i < results.length; i++){
+        
+            // Creates a div to hold the giph
+            let giphDiv = $("<div class='giph-div'>");
+            var rating = results[i].rating;
+            var title = results[i].title;
+            if(title === "") {
+              title = "N/A"
             }
-
-            $(".gif").on("click", function() {
-                var state = $(this).attr('data-state');      
-                    if (state === 'still') {
-                    $(this).attr("src", $(this).attr('data-animate'));
-                    $(this).attr("data-state", "animate");
-                    } else {
-                        $(this).attr("src", $(this).attr("data-still"));
-                        $(this).attr("data-state", "still");
-                    }
-                });
+            let pOne = $('<p>').text('Name: ' + title + ' | ' + 'Rating: ' + rating);
+            giphDiv.append(pOne);
             
+            let pic = $('<img>').attr({"src": results[i].images.fixed_height_still.url, 
+                                        "data-state": "still",
+                                        "data-still": results[i].images.fixed_height_still.url,
+                                        "data-animate": results[i].images.fixed_height.url,
+                                        "class": "gif"});
 
-        });
+            let picAni = $('<img>').attr("src", results[i].images.fixed_height.url);
 
-      }
+            console.log(results);
+            
+            giphDiv.append(pic);
 
-      // Function for displaying giph data
-      function renderButtons() {
-
-        // Deletes the giphs prior to adding new giphs
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-        // Loops through the array of giphs
-        for (var i = 0; i < giphs.length; i++) {
-
-          // Then dynamicaly generates buttons for each giph in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adds a class of giph to our button
-          a.addClass("giph");
-          // Added a data-attribute
-          a.attr("data-name", giphs[i]);
-          // Provided the initial button text
-          a.text(giphs[i]);
-          // Added the button to the buttons-view div
-          $("#buttons-view").append(a);
+              $("#giph-container").prepend(giphDiv);
         }
-      }
 
-      // This function handles events where the add giph button is clicked
-      $("#add-giph").on("click", function(event) {
-        event.preventDefault();
+        $(".gif").on("click", function() {
+            var state = $(this).attr('data-state');      
+                if (state === 'still') {
+                $(this).attr("src", $(this).attr('data-animate'));
+                $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            });
+        
 
-        // This line of code will grab the input from the textbox
-        var giph = $("#giph-input").val().trim();
+    });
 
-        // The giph from the textbox is then added to our array
-        giphs.push(giph);
+  }
 
-        // Calling renderButtons which handles the processing of our giph array
-        renderButtons();
-      });
+  // Function for displaying giph data
+  function renderButtons() {
 
-      // Adding click event listeners to all elements with a class of "giph"
-      $(document).on("click", ".giph", displayGiphRating);
+    // Deletes the giphs prior to adding new giphs
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+    // Loops through the array of giphs
+    for (var i = 0; i < giphs.length; i++) {
 
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
+      // Then dynamicaly generates buttons for each giph in the array
+      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      var a = $("<button>");
+      // Adds a class of giph to our button
+      a.addClass("giph");
+      // Added a data-attribute
+      a.attr("data-name", giphs[i]);
+      // Provided the initial button text
+      a.text(giphs[i]);
+      // Added the button to the buttons-view div
+      $("#buttons-view").append(a);
+    }
+  }
+
+  // This function handles events where the add giph button is clicked
+  $("#add-giph").on("click", function(event) {
+    event.preventDefault();
+
+    // This line of code will grab the input from the textbox
+    var giph = $("#giph-input").val().trim();
+
+    // The giph from the textbox is then added to our array
+    giphs.push(giph);
+
+    // Calling renderButtons which handles the processing of our giph array
+    renderButtons();
+  });
+
+  // Adding click event listeners to all elements with a class of "giph"
+  $(document).on("click", ".giph", displayGiphRating);
+
+  // Calling the renderButtons function to display the intial buttons
+  renderButtons();
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
-Giphy Machine App
+#Giphy Machine App
 
-Allows users to create buttons and name with with search terms.
-WHen clicked, the app searches Giphy.com and returns 10 gifs that
-will populate on the page with rating information.
+[Check it out!](https://coombapace.github.io/giphymachine/)
+
+Invites users to create buttons that, when clicked, will search Giphy.com and returns 10 gifs that
+will populate on the page with rating information, name based on the name
+of the buttons.
